1ca0e9c4bba6935d3eb7784f18f6213444f5e617
daartv <daartv@gmail.com>
2017-02-21 15:22:47 -0800

    Pass Songs tests

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -1,6 +1,22 @@
 // Songs.js - Defines a backbone collection class for songs.
 var Songs = Backbone.Collection.extend({
 
-  model: SongModel
+  model: SongModel,
+
+
+  initialize: function() {
+    var context = this;
+    $.ajax({
+      url: 'http://parse.sfm6.hackreactor.com/mytunes/classes/songs',
+      type: 'GET',
+      contentType: 'application/json',
+      success: function(data) {
+        this.add(data.results);
+      }.bind(this),
+      error: function() {
+        console.error('Error in retrieving data.');
+      }
+    });
+  },
 
 });
