09df580be3965f528f49fd8eaa7fe7046d3621be
daartv <daartv@gmail.com>
2017-02-20 19:27:00 -0800

    Pass SongQueue tests

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,25 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
-  }
+    this.on('add', function() {
+      if (this.length === 1) {
+        this.playFirst();  
+      }  
+    });
 
+    this.on('ended', function() {
+      this.shift();
+      //check if there any more songs
+      if (this.length > 0) {
+        //playFirst if there are
+        this.playFirst();
+      }
+    });
+
+    this.on('dequeue', this.remove);
+  },
+
+  playFirst: function() {
+    this.at(0).play();
+  }
 });
