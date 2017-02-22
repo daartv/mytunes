cc78c8e1d1546bb029f2a97ff389763b168a1184
daartv <daartv@gmail.com>
2017-02-21 19:19:07 -0800

    Refactor DOM event listener

--- client/views/PlayerView.js
+++ client/views/PlayerView.js
@@ -5,10 +5,16 @@ var PlayerView = Backbone.View.extend({
   // see: https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Using_HTML5_audio_and_video
   el: '<audio controls autoplay />',
 
-  initialize: function() {
-    $(this.el).on('ended', function() {
+  events: { // Listens for events on the DOM
+    'ended': function() {
       this.model.ended();
-    }.bind(this));
+    }
+  }, 
+
+  initialize: function() {
+  /*  $(this.el).on('ended', function() {
+      this.model.ended();
+    }.bind(this)); */
   },
 
   setSong: function(song) {
